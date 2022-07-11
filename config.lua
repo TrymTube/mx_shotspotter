6e7d9a337a199a031afe0f193dc88ee7b208f4f7
Max Husky <62698394+TrymTube@users.noreply.github.com>
2022-07-11 12:06:55 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,6 +3,7 @@ Config = {}
 Config.EnableSilencerDetection = true -- when true enables the detection for Weapons with silencer
 
 Config.NotifyType = 2 -- 1 = ESX Notification, 2 = Export Notification ( Only use 2 when you know how to change the export in notify.lua )
+Config.UsePhoneDispatch = false -- if true use the event down below
 
 Config.EnableBlips = true -- Enable or Disable blips
 Config.BlipRemoveTime = 60 -- Time when the blips get removed in seconds
@@ -26,3 +27,8 @@ Config.BlacklistedWeapons = { -- Enter Blacklisted Weapons in here
 	`WEAPON_HAZARDCAN`,
 	`WEAPON_FIREEXTINGUISHER`
 }
+
+RegisterNetEvent('mx_shotspotter:phoneDispatch')
+AddEventHandler('mx_shotspotter:phoneDispatch', function(coords, currentWeapon) -- If Config.UsePhoneDispatch = true insert your Phone Dispatch Trigger in here
+	
+end)
